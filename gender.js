515407131a5aab321b6e1fd44a3c54e31f778643
Zlaya19 <zlatangavranovic1906@gmail.com>
2022-12-26 11:36:32 +0100

    adding images

--- gender.js
+++ gender.js
@@ -1,10 +1,13 @@
 const input = document.getElementById('input');
 const output = document.getElementById('output');
+const body = document.getElementsByTagName('body')[0];
+const container = document.getElementById('container');
 
 let nameValue = '';
 
 input.addEventListener('keydown', (e) => {
     if(e.key === 'Enter'){
+        /* body.style.backgroundImage = none; */
         let xhr = new XMLHttpRequest();
         xhr.open('GET',`https://api.genderize.io/?name=${input.value}`, true);
 
@@ -15,6 +18,7 @@ input.addEventListener('keydown', (e) => {
                 output.innerHTML = `<span1>${nameValue.name} </span1>`;
                 output.innerHTML += `<span>gender:</span>`+ nameValue.gender + "<br>";
                 output.innerHTML += `<span>probability:</span>`+ nameValue.probability + "<br>";
+                container.style.backgroundImage = `url('images/${nameValue.gender}.png')`;
                 console.log(nameValue);
             }
         }
